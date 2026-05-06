738441b5035090e146d3c509e39bb8a7ac4c477c
Saravana <saravana.r@digifox.studio>
2026-05-06 10:50:29 +0530

    Fix white screen for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Retire/',
+  // Use a relative base path to ensure it works on both Vercel and GitHub Pages
+  base: './',
 })
